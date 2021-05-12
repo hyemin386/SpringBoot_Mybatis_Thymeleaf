0cb3844450ddf7938237fa9c13cbb50e1aebbd4c
hyemin386 <ha_ha24@naver.com>
2021-05-12 12:04:37 +0900

    notice insert

--- src/main/java/com/hm/j1/board/notice/NoticeController.java
+++ src/main/java/com/hm/j1/board/notice/NoticeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.ModelAndView;
@@ -39,6 +40,18 @@ public class NoticeController {
 		mv.addObject("vo", boardVO);
 		mv.setViewName("board/select");
 		return mv;
+	}
+	
+	@GetMapping("insert")
+	public String setInsert() throws Exception {
+		return "board/insert";
+	}
+	
+	@PostMapping("insert")
+	public String setInsert(BoardVO boardVO) throws Exception {
+		int result = noticeService.setInsert(boardVO);
+		
+		return "redirect:./list";
 	}
 	
 }
--- src/test/java/com/hm/j1/board/notice/NoticeMapperTest.java
+++ src/test/java/com/hm/j1/board/notice/NoticeMapperTest.java
@@ -17,6 +17,19 @@ class NoticeMapperTest {
 	private NoticeMapper noticeMapper;
 	
 	@Test
+	void setInsertTest() throws Exception {
+		for(int i=5;i<106;i++) {
+			BoardVO boardVO = new BoardVO();
+			boardVO.setTitle("title"+i);
+			boardVO.setContents("contents"+i);
+			boardVO.setWriter("writer"+i);
+			noticeMapper.setInsert(boardVO);
+			Thread.sleep(500);
+		}
+		System.out.println("Finish");
+	}
+	
+	//@Test
 	void getListTest() throws Exception {
 		List<BoardVO> ar = noticeMapper.getList();
 		for(BoardVO boardVO : ar) {
