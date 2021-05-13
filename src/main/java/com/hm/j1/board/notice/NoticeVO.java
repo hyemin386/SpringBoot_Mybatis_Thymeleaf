a64d9f732d86ea5896e97edd50c423ec515ad491
hyemin386 <ha_ha24@naver.com>
2021-05-13 16:10:22 +0900

    notice select(+file)

--- src/main/java/com/hm/j1/board/notice/NoticeVO.java
+++ src/main/java/com/hm/j1/board/notice/NoticeVO.java
@@ -1,7 +1,14 @@
 package com.hm.j1.board.notice;
 
+import java.util.List;
+
+import com.hm.j1.board.BoardFileVO;
 import com.hm.j1.board.BoardVO;
 
+import lombok.Data;
+
+@Data
 public class NoticeVO extends BoardVO {
 
+	private List<BoardFileVO> files;
 }
