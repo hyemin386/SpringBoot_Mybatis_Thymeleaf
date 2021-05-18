13fdd67b7a4f3da73473f09cec8a8d1035d6d302
hyemin386 <ha_ha24@naver.com>
2021-05-18 12:42:46 +0900

    interceptor test

--- src/main/java/com/hm/j1/config/InterceptorConfig.java
+++ src/main/java/com/hm/j1/config/InterceptorConfig.java
@@ -7,19 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.hm.j1.interceptor.TestInterceptor;
 
-@Configuration
+@Configuration //root-context, servlet-context 역할
 public class InterceptorConfig implements WebMvcConfigurer {
 
 	@Autowired
-	private TestInterceptor TestInterceptor;
+	private TestInterceptor testInterceptor;
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		//Interceptor bean을 등록
-		registry.addInterceptor(TestInterceptor)
-		.addPathPatterns("/notice/**"); //앞에 registy 생략되있음		
+		registry.addInterceptor(testInterceptor)
+		//.addPathPatterns("/notice/**") //앞에 registy 생략되있음	
+		.addPathPatterns("/qna/**");
+		//.excludePathPatterns("/notice/select"); //notice/select 제외시키기
 		//어떤 URL이 왔을 때 발생시킬 것인지 설정
 		
-		WebMvcConfigurer.super.addInterceptors(registry);
+		//WebMvcConfigurer.super.addInterceptors(registry);
 	}
 }
--- src/main/java/com/hm/j1/interceptor/TestInterceptor.java
+++ src/main/java/com/hm/j1/interceptor/TestInterceptor.java
@@ -1,11 +1,15 @@
 package com.hm.j1.interceptor;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hm.j1.member.MemberVO;
 
@@ -15,8 +19,9 @@ public class TestInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
-		HttpSession session = request.getSession();
+		//Controller 진입 전 실행
+		System.out.println("Controller 진입 전");
+		/*HttpSession session = request.getSession();
 		Object obj = session.getAttribute("member"); 		
 		MemberVO memberVO = null;
 		boolean result = false;
@@ -29,9 +34,34 @@ public class TestInterceptor implements HandlerInterceptor {
 		
 		if(!result) {
 			response.sendRedirect("/member/login");
-		}
+		}*/
 		
-		return result;
+		return true;
 	}
 	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		//Controller 종료 후 실행
+		System.out.println("Controller 종료 후");
+		Map<String, Object> map = modelAndView.getModel();
+		Iterator<String> it = map.keySet().iterator();
+		
+		while(it.hasNext()) {
+			String key = it.next();
+			System.out.println(key);
+			System.out.println(map.get(key));
+		}
+		
+		//view 경로를 확인하거나 변경 가능
+		//modelAndView.getViewName();
+		//modelAndView.setViewName("");
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		//Client 전송 전
+		System.out.println("Client 전송 전");
+	}
 }
